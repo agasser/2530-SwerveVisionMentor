9dcf37fb18b25b21407b3017be22cf259d010309
Andrew Gasser <agasser01@hotmail.com>
2023-11-18 22:28:41 -0600

    Untested merge of savage branch to Development

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -28,7 +28,7 @@ public final class Constants {
 
   public static final class DrivetrainConstants {
     public static final double WHEEL_DIAMETER = Units.inchesToMeters(4);
-    public static final double STEERING_GEAR_RATIO = 1.d / (150d / 7d);
+    public static final double STEERING_GEAR_RATIO = (14.0 / 50.0) * (10.0 / 60.0);
     public static final double DRIVE_GEAR_RATIO = 1.d / 6.75d;
 
     public static final double DRIVE_ROTATION_TO_METER = DRIVE_GEAR_RATIO * Math.PI * WHEEL_DIAMETER;
@@ -46,33 +46,37 @@ public final class Constants {
     public static final int FL_DRIVE_ID = 2;
     public static final int FL_STEER_ID = 1;
     public static final int FL_ABSOLUTE_ENCODER_PORT = 1;
-    public static final double FL_OFFSET_RADIANS = Units.degreesToRadians(26.543) + Math.PI * 0.5d;
-    public static final boolean FL_ABSOLUTE_ENCODER_REVERSED = true;
-    public static final boolean FL_MOTOR_REVERSED = false;
+    public static final double FL_OFFSET_ROTATIONS = -0.073730;
+    public static final boolean FL_ABSOLUTE_ENCODER_REVERSED = false;
+    public static final boolean FL_DRIVE_MOTOR_REVERSED = true;
+    public static final boolean FL_STEER_MOTOR_REVERSED = true;
 
     // --------- Front Right Module --------- \\
     public static final int FR_DRIVE_ID = 4;
     public static final int FR_STEER_ID = 3;
     public static final int FR_ABSOLUTE_ENCODER_PORT = 4;
-    public static final double FR_OFFSET_RADIANS = Units.degreesToRadians(336.621) + Math.PI * 0.5d;
-    public static final boolean FR_ABSOLUTE_ENCODER_REVERSED = true;
-    public static final boolean FR_MOTOR_REVERSED = false;
+    public static final double FR_OFFSET_ROTATIONS = -0.935547;
+    public static final boolean FR_ABSOLUTE_ENCODER_REVERSED = false;
+    public static final boolean FR_DRIVE_MOTOR_REVERSED = true;
+    public static final boolean FR_STEER_MOTOR_REVERSED = true;
 
     // --------- Back Right Module --------- \\
     public static final int BR_DRIVE_ID = 6;
     public static final int BR_STEER_ID = 5;
     public static final int BR_ABSOLUTE_ENCODER_PORT = 2;
-    public static final double BR_OFFSET_RADIANS = Units.degreesToRadians(54.404) + Math.PI * 0.5d;
-    public static final boolean BR_ABSOLUTE_ENCODER_REVERSED = true;
-    public static final boolean BR_MOTOR_REVERSED = false;
+    public static final double BR_OFFSET_ROTATIONS = -0.150879;
+    public static final boolean BR_ABSOLUTE_ENCODER_REVERSED = false;
+    public static final boolean BR_DRIVE_MOTOR_REVERSED = true;
+    public static final boolean BR_STEER_MOTOR_REVERSED = true;
 
     // --------- Back Left Module --------- \\
     public static final int BL_DRIVE_ID = 8;
     public static final int BL_STEER_ID = 7;
     public static final int BL_ABSOLUTE_ENCODER_PORT = 3;
-    public static final double BL_OFFSET_RADIANS = Units.degreesToRadians(320.801) + Math.PI * 0.5d;
-    public static final boolean BL_ABSOLUTE_ENCODER_REVERSED = true;
-    public static final boolean BL_MOTOR_REVERSED = false;
+    public static final double BL_OFFSET_ROTATIONS = -0.888916;
+    public static final boolean BL_ABSOLUTE_ENCODER_REVERSED = false;
+    public static final boolean BL_DRIVE_MOTOR_REVERSED = true;
+    public static final boolean BL_STEER_MOTOR_REVERSED = true;
 
     public static final double MAX_MODULE_VELOCITY = 4.8;
     public static final double MAX_ROBOT_VELOCITY = 4.8;
@@ -83,10 +87,10 @@ public final class Constants {
     public static final double WHEEL_BASE = Units.inchesToMeters(18.75);
 
     public static final SwerveDriveKinematics KINEMATICS = new SwerveDriveKinematics(
-        new Translation2d(TRACK_WIDTH / 2.0, WHEEL_BASE / 2.0),
-        new Translation2d(TRACK_WIDTH / 2.0, -WHEEL_BASE / 2.0),
-        new Translation2d(-TRACK_WIDTH / 2.0, WHEEL_BASE / 2.0),
-        new Translation2d(-TRACK_WIDTH / 2.0, -WHEEL_BASE / 2.0));
+        new Translation2d(-TRACK_WIDTH / 2.0, -WHEEL_BASE / 2.0), // Back Right
+        new Translation2d(TRACK_WIDTH / 2.0, -WHEEL_BASE / 2.0), // Front Right
+        new Translation2d(-TRACK_WIDTH / 2.0, WHEEL_BASE / 2.0), // Back Left
+        new Translation2d(TRACK_WIDTH / 2.0, WHEEL_BASE / 2.0)); // Front left
 
     public static final double XY_SPEED_LIMIT = 0.8;
     public static final double Z_SPEED_LIMIT = 1.0;
